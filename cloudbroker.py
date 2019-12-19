4ca7dc7a587db9ef8e0a679f7c81f7245216e695
Gustavobbs <36171903+Gustavobbs@users.noreply.github.com>
2019-12-19 17:34:57 -0300

    Add files via upload

---
+++ cloudbroker.py
@@ -0,0 +1,95 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from bson import json_util
+from pprint import pprint
+import json
+import bson
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = "mongodb://admin:sd1234@ds159184.mlab.com:59184/sd"
+mongo = PyMongo(app)
+
+PROV_URL = 'http://127.0.0.1:5001/'
+@app.route('/')
+def main():
+    return 'Welcome to SD'
+
+@app.route('/provedor/cadastrar/<pid>',methods=['POST'])
+def init_provedor(pid):
+    data = request.get_json()
+    mongo.db['vm'].insert_one(
+        {
+            'pid': pid,
+            'vcpu': data['vcpu'],
+            'ram': data['ram'],
+            'hd': data['hd'],
+            'preco':data['preco'],  
+            'usando': 'False',
+            'reserva': '0'
+        })
+
+    return jsonify({'Ok': True})
+
+@app.route('/provedor/search/<pid>', methods=['POST'])
+def search_provedor(pid):
+    data = request.get_json()
+    busca = mongo.db['vm'].find(
+        {
+            'pid': pid
+        }
+    )
+    return bson.json_util.dumps(busca) 
+
+@app.route('/search',methods=['POST'])
+def search_vm():
+    data = request.get_json()
+    busca = mongo.db['vm'].find(
+        {
+            'vcpu':{'$gte': data['vcpu']},
+            'ram': {'$gte': data['ram']},
+            'hd': {'$gte': data['hd']},
+            'usando': 'False'
+        }
+    )
+    
+    return bson.json_util.dumps(busca)
+
+@app.route('/cliente/reservar/<pid>', methods=['POST'])
+def reserve_vm(pid):
+    data = request.get_json()
+    mongo.db['vm'].update_one(
+        {
+            'vcpu':data['vcpu'],
+            'ram': data['ram'],
+            'hd': data['hd'],
+        }, {'$set': {'usando':'True', 'reserva':pid}}
+    )
+    
+    return jsonify({'Ok': True})
+
+@app.route('/cliente/consultar/<pid>', methods=['POST'])
+def consultar_vm(pid):
+    data = request.get_json()
+    busca = mongo.db['vm'].find(
+        {
+            'reserva':pid,
+            'usando':'True'
+        }
+    )
+    return bson.json_util.dumps(busca)
+
+@app.route('/cliente/liberar', methods=['POST'])
+def liberar_vm():
+    data = request.get_json()
+    mongo.db['vm'].update_one(
+        {
+            'vcpu':data['vcpu'],
+            'ram':data['ram'],
+            'hd':data['hd'],
+        }, {'$set': {'usando':'False', 'reserva':'0'}}
+    )
+
+    return jsonify({'Ok': True})
+
+if __name__ == '__main__':
+    app.run()
---
+++ set_provedor.py
@@ -0,0 +1,72 @@
+import requests
+import json
+import sys
+
+# CLOUD_URL = 'http://127.0.0.1:5000/'
+CLOUD_URL = 'https://sd-broker.herokuapp.com/'
+PROV_URL = 'http://127.0.0.1:5000/'
+
+class Provedor:
+    def __init__(self):
+        self.quantidade = 0
+        self.recurso = {}
+
+    def menu(self):
+        while True:
+            print ('1. Divulgar recursos')
+            print ('2. Pesquisar maquinas')
+            print ('3. Sair')
+            opc = int(input(''))
+
+            if opc == 1:
+             
+                response = self.divulgar()
+
+                if response['Ok'] == True:
+                    print ('\n--> Recurso divulgado com sucesso.\n')
+                else:
+                    print ('\n--> Erro ao divulgar recurso.\n')
+
+            elif opc == 2:
+                response = self.pesquisar()
+                for vms in response:
+                    print("\tID: "),
+                    print(vms['_id'])
+                    print("\tvCPU: " + vms['vcpu'])
+                    print("\tHD: " + vms['hd'])
+                    print("\tRAM: " + vms['ram'])
+                    print("\tPreco: " + vms['preco'] + "\n")
+                        
+            elif opc == 3:
+                return 0
+
+    def divulgar(self):
+        
+        pid = input('Informe o seu id: ')
+        recursos = {
+            'vcpu':'',
+            'ram':'',
+            'hd':'',
+            'preco':''
+        }
+        recursos['vcpu'] = str(input('Quantidade de vCPUs: '))
+        recursos['ram'] = str(input('Quantidade de memoria RAM (em GB): '))
+        recursos['hd'] = str(input('Quantidade de disco (HD, em GB): '))
+        recursos['preco'] = str(input('Preco do recurso (em R$): '))
+    
+        return self.postRequest(recursos, PROV_URL + 'provedor/cadastrar/' + str(pid))
+
+    def pesquisar(self):
+        pid = input('Informe o seu id: ')
+        recursos = {}
+        return self.postRequest(recursos, PROV_URL + 'provedor/search/' + str(pid))
+
+    def postRequest(self, data, url):
+        headers = {'Content-Type': 'application/json',}
+        post = requests.post(url=url, data=json.dumps(data), headers=headers)
+
+        return post.json()
+
+if __name__ == '__main__':
+    p = Provedor()
+    p.menu()
